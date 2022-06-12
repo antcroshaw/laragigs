7792e77b9c3208c6bbff1f66bf6f97e09db18054
antcroshaw@outlook.com <vaHwaj-gustet-qoqqi6>
2022-06-12 12:52:16 +0100

    added search scope filter and clockwork

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,9 +9,9 @@ class ListingController extends Controller
 {
     // show all listings
     public function index() {
-
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter
+            (request(['tag', 'search']))->get()
         ]);
     }
 
